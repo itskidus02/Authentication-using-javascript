f8186f4446b178348205f1b6325238e72b71987b
itskidus02 <kidusasebe2019@gmail.com>
2023-09-26 21:49:33 +0300

    1st level auth,( logging in if the password from registry matches)

---
+++ app.js
@@ -0,0 +1,84 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+// mongoose user schema
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+// user model using the user schema
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//creates new user from the register page
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+
+//from login.ejs accepts credentials, if they match it renders secrets.ejs
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log("server is up");
+});
